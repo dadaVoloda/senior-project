a46af106b2de1a062f96c271ffadb405ecaab5d2
Vladimir Che <vladimirchukarev@gmail.com>
2023-02-01 10:49:04 +0300

    start FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
 import { Route, Routes, Link } from 'react-router-dom'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import { Suspense } from 'react'
-
-import { useTheme } from './theme/useTheme'
-import { classNames } from './helpers/classNames/classNames'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { MainPage } from 'pages/MainPage'
+import { AboutPage } from 'pages/AboutPage'
 
 import './styles/index.scss'
 
@@ -18,8 +17,8 @@ const App = () => {
       <Link to='/about'>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageAsync />} />
-          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/' element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
